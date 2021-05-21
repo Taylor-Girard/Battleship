4e829a6ba219aba33bb609b95ec06ad7c9bcff7d
Taylor-Girard <686taytay@gmail.com>
2021-05-21 14:09:54 -0700

    added input for player name

--- Battleship Game.java
+++ Battleship Game.java
@@ -1,9 +1,22 @@
 
-public class BattleshipGame{
+import java.util.Scanner;
+
+class BattleshipGame{
 
     public static void main(String[] args) {
+        
+        //initialize all variables/arrays that will be used
+        String player1;
+        
         //print welcome message
-        System.out.println("Welcome to Battleship!")
+        System.out.println("Welcome to Battleship!");
+
+        //get player name
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        player1 = input.nextLine();
+        System.out.println("Hi " + player1 + ". Thank you for playing!");
+
 
        
 
